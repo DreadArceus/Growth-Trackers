04b833dedab9ebe612239576a8b999281432b715
Mayank Talwar <dreadarceus@gmail.com>
2020-12-25 23:19:28 +0530

    refresh

--- Competitive Setup/IO.cpp
+++ Competitive Setup/IO.cpp
@@ -6,13 +6,43 @@ int solve(int n)
   int x = 0;
   return x;
 }
+template<class Ti, class To>
+void func(Ti &i, To &o, ifstream &c)
+{
+  int n = 0;
+  i.ignore(3, '\n');
+  i >> n;
+  int var = solve(n);
+  if(c.is_open())
+  {
+    int ans = 0;
+    c >> ans;
+    c.ignore(3, '\n');
+    cout << "Testing... ";
+    if(var != ans)
+    {
+      cout << "Wrong Answer\n";
+      o << "x ";
+    }
+    else
+    {
+      cout << "Passed\n";
+    }
+  }
+  o << var << "\n";
+}
 int main()
 {
-  ifstream inFile("/Users/dreadarceus/github/.../input.txt");
-  ofstream outFile("/Users/dreadarceus/github/.../output.txt");
-  outFile << "...\n";
-  int n = 0;
-  inFile >> n;
-  int var = solve(n);
-  outFile << var << "\n";
+  ifstream checkFile, inFile("/Users/dreadarceus/github/.../input.txt");
+  ofstream outFile;
+  if(inFile.is_open())
+  {
+    checkFile.open("/Users/dreadarceus/github/.../correctOutput.txt");
+    outFile.open("/Users/dreadarceus/github/.../output.txt");
+    func(inFile, outFile, checkFile);
+  }
+  else
+  {
+    func(cin, cout, checkFile);
+  }
 }
